80d1c8ff9fd2d380a0b623040549dcaf7e08fc65
Swetha1999 <79907153+Swetha1999@users.noreply.github.com>
2021-03-02 16:27:05 +0530

    Add files via upload

---
+++ Named&Optional_Parameter/Named&Optional_Parameter/NamedParametersHandsOn/NamedParameter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParametersHandsOn
+{
+    class NamedParameter
+    {
+        public void GetCohortDetails(string c_name,int genc_count,string mode,string track,string curr_module)
+        {
+            Console.WriteLine("It is {0} with {1} GenCs undergoing training for {2} thru {3}. The current module of training is {4}",c_name,genc_count,track,mode,curr_module);
+        }
+        static void Main(string[] args)
+        {
+            NamedParameter p = new NamedParameter();
+            p.GetCohortDetails("ADM20DF003", 21, "OBL", ".Net", "Advance C#");
+            p.GetCohortDetails(".Net", 21, "OBL", "ADM20DF003", "Advance C#");
+            p.GetCohortDetails(track:".Net",mode:"OBL",c_name:"ADM20DF003",curr_module:"Advance C#",genc_count:21);
+
+            OptionalParameter op = new OptionalParameter();
+            op.OrderDetails("Flipkart", "Samsung Mobile");
+            op.OrderDetails("Flipkart", "Samsung Mobile",3,false);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Named&Optional_Parameter/Named&Optional_Parameter/NamedParametersHandsOn/OptionalParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParametersHandsOn
+{
+    class OptionalParameter
+    {
+        public void OrderDetails(string sellerName,string prodName,int quantity=1,bool isReturnable=true)
+        {
+            Console.WriteLine("Here is the order detail – {0} number of {1} by {2} is ordered. It’s returnable status is {3}", quantity, prodName, sellerName, isReturnable);
+        }
+    }
+}
