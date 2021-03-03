0285e499fe2c58fa3b28f2f68ba97e59bfae8f6a
Swetha1999 <79907153+Swetha1999@users.noreply.github.com>
2021-03-03 21:58:11 +0530

    Add files via upload

---
+++ WindowsFormsHandson/WindowsFormsHandson/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsHandson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int CountChars()
+        {
+            int count = 0;
+            using (StreamReader sr = new StreamReader("C:\\Users\\swetha\\source\\repos\\WindowsFormsHandson\\WindowsFormsHandson\\Text.txt"))
+            {
+                string content = sr.ReadToEnd();
+                count = content.Length;
+                Thread.Sleep(5000);
+            }
+            return count;
+        }
+        
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(CountChars);
+            task.Start();
+
+            label2.Text = "Processing File. Please wait....";
+            int count = await task;
+            label2.Text = count.ToString() + " characters in file";
+
+        }
+    }
+}
