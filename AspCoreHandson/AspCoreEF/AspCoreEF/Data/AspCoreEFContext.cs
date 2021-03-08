eecd2aadd69b727b808e4ae98c1233932cdd3328
Swetha1999 <swethanarasimhan22@gmail.com>
2021-03-08 11:19:21 +0530

    Swetha

---
+++ AspCoreHandson/AspCoreEF/AspCoreEF/Data/AspCoreEFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AspCoreEF.Models;
+
+namespace AspCoreEF.Data
+{
+    public class AspCoreEFContext : DbContext
+    {
+        public AspCoreEFContext (DbContextOptions<AspCoreEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AspCoreEF.Models.Account> Account { get; set; }
+    }
+}
