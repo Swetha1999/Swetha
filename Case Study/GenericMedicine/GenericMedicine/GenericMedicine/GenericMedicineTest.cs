68cc6b665cb3dbbbcb14296be0998c9040305c9b
Swetha1999 <swethanarasimhan22@gmail.com>
2021-03-11 00:34:25 +0530

    Swetha

---
+++ Case Study/GenericMedicine/GenericMedicine/GenericMedicine/GenericMedicineTest.cs
@@ -0,0 +1,111 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMedicine
+{
+    [TestFixture]
+    class GenericMedicineTest
+    {
+        //public static Medicine m;
+        [Test]
+        [TestCase("","name","medicine","2021/09/07",70.00)]
+        public void GenericNameIsNullOrEmpty(string name,string genericMedicineName,string composition,DateTime expiryDate,double price)
+        {
+            Program p = new Program();
+            var exception = Assert.Throws<Exception>(() => p.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, price));
+            Assert.AreEqual("Medicine name cannot be empty. Please provide valid value", exception.Message);
+        }
+        [Test]
+        [TestCase("abc", "name", "medicine", "2021/09/07",-1)]
+        public void GenericPriceIsNullOrEmpty(string name, string genericMedicineName, string composition, DateTime expiryDate, double price)
+        {
+            Program p = new Program();
+            var exception = Assert.Throws<Exception>(() => p.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, price));
+            Assert.AreEqual("Incorrect value for Medicine price per strip. Please provide valid value", exception.Message);
+        }
+        [Test]
+        [TestCase("abc", "name", "medicine", "2020/09/07", 70.00)]
+        public void GenericDateIsNullOrEmpty(string name, string genericMedicineName, string composition, DateTime expiryDate, double price)
+        {
+            Program p = new Program();
+            var exception = Assert.Throws<Exception>(() => p.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, price));
+            Assert.AreEqual("Incorrect expiry date. Please provide valid value", exception.Message);
+        }
+        [Test]
+        [TestCase("Sdf","name","medicine","2021/09/07",80.00)]
+        public void CreateMedicineDetail_WhenCalled_CreateAndReturnMedicine(string name, string genericMedicineName, string composition, DateTime expiryDate, double price)
+        {
+            Program p = new Program();
+            Medicine m = p.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, price);
+            Assert.That(m.PricePerStrip,Is.EqualTo(price));
+        }
+        public Medicine get(int n)
+        {
+            if (n == 1)
+            {
+                return new Medicine()
+                {
+
+                    Name = "medicine",
+                    GenericMedicineName = "abv",
+                    Composition = "mkl",
+                    ExpiryDate = new DateTime(2021, 09, 08),
+                    PricePerStrip = 90.00
+                };
+            }
+            else{
+                return null;
+            }
+
+        }
+        [Test]
+        [TestCase(1,"2021/08/07","Chennai",1)]
+        public void  CreateCartonDetail_ObjectNullOrNot(int medicineStripCount, DateTime launchDate, string retailerAddress,int n)
+        {
+            Program p = new Program();
+            Medicine m;
+            m = get(n);
+            CartonDetail res = p.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, m);
+            Assert.That(res.MedicineStripCount, Is.EqualTo(medicineStripCount));
+            Assert.That(res.LaunchDate ,Is.EqualTo(launchDate));
+            Assert.That(res.RetailerAddress, Is.EqualTo(retailerAddress));
+        }
+        [Test]
+        [TestCase(-1, "2021/08/07", "Chennai", 1)]
+        public void CreateCartonDetail_medicineStripCountLessThanZero(int medicineStripCount, DateTime launchDate, string retailerAddress, int n)
+        {
+            Program p = new Program();
+            Medicine m;
+            m = get(n);
+             
+            var exception = Assert.Throws<Exception>(() => p.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, m));
+            Assert.AreEqual("Incorrect strip count. Please check", exception.Message);
+        }
+        [Test]
+        [TestCase(1, "2020/08/07", "Chennai", 1)]
+        public void CreateCartonDetail_LauchDateLessThanZero(int medicineStripCount, DateTime launchDate, string retailerAddress, int n)
+        {
+            Program p = new Program();
+            Medicine m;
+            m = get(n);
+
+            var exception = Assert.Throws<Exception>(() => p.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, m));
+            Assert.AreEqual("Incorrect launch date. Please provide valid value", exception.Message);
+        }
+        [Test]
+        [TestCase(1, "2021/08/07", "Chennai", 0)]
+        public void CreateCartonDetail_ObjectNull(int medicineStripCount, DateTime launchDate, string retailerAddress, int n)
+        {
+            Program p = new Program();
+            Medicine m;
+            m = get(n);
+            CartonDetail res = p.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, m);
+            
+            Assert.That(res, Is.EqualTo(null));
+        }
+    }
+}
---
+++ Case Study/MyFoodSupply/MyFoodSupply/MyFoodSupply/FoodSupplyTest.cs
@@ -0,0 +1,111 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFoodSupply
+{
+    [TestFixture]
+    class FoodSupplyTest
+    {
+        [Test]
+        [TestCase("",2, "2021/09/07", 70.00)]
+        public void FoodItemName_IsNullOrEmpty(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Program p = new Program();
+            var exception = Assert.Throws<Exception>(() => p.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.AreEqual("Dish name cannot be empty. Please provide valid value", exception.Message);
+        }
+        [Test]
+        [TestCase("abc", 2, "2021/09/07", -1)]
+        public void FoodItemPrice_IsNullOrEmpty(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Program p = new Program();
+            var exception = Assert.Throws<Exception>(() => p.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.AreEqual("Incorrect value for dish price. Please provide valid value", exception.Message);
+        }
+        [Test]
+        [TestCase("abc",2 , "2020/09/07", 70.00)]
+        public void FoodItemDate_LessThan_PresentDate(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Program p = new Program();
+            var exception = Assert.Throws<Exception>(() => p.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.AreEqual("Incorrect expiry date. Please provide valid value", exception.Message);
+        }
+        [Test]
+        [TestCase("Sdf", 2, "2021/09/07", 80.00)]
+        public void CreateFoodItemDetail_WhenCalled_CreateAndReturnFoodItem(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Program p = new Program();
+            FoodDetail m = p.CreateFoodDetail(name, dishType, expiryDate, price);
+            Assert.That(m.Price, Is.EqualTo(price));
+        }
+        public FoodDetail get(int n)
+        {
+            if (n == 1)
+            {
+                return new FoodDetail()
+                {
+
+                    Name = "medicine",
+                    DishType = FoodDetail.Category.MainDish,
+                    ExpiryDate = new DateTime(2021, 09, 08),
+                    Price = 90.00
+                };
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        [Test]
+        [TestCase(1, "2021/08/07", "abv",20.00, 1)]
+        public void CreateSupllyDetail_ObjectNotNull(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge, int n)
+        {
+            Program p = new Program();
+            FoodDetail m;
+            m = get(n);
+            SupplyDetail res = p.CreateSupplyDetail(foodItemCount, requestDate,sellerName,packingCharge, m);
+            Assert.That(res.Count, Is.EqualTo(foodItemCount));
+            Assert.That(res.RequestDate, Is.EqualTo(requestDate));
+            Assert.That(res.SellerName, Is.EqualTo(sellerName));
+            Assert.That(res.FoodItem, Is.EqualTo(m));
+        }
+        [Test]
+        [TestCase(-1, "2021/08/07", "bvc", 20.00, 1)]
+        public void CreateSupllyDetail_FoodItemCountLessThanZero(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge, int n)
+        {
+            Program p = new Program();
+            FoodDetail m;
+            m = get(n);
+
+            var exception = Assert.Throws<Exception>(() => p.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, m));
+            Assert.AreEqual("Incorrect food item count. Please check", exception.Message);
+        }
+        [Test]
+        [TestCase(1, "2020/08/07", "bvc", 20.00, 1)]
+        public void CreateSupplyDetail_LauchDateLessThan_CurrentDate(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge, int n)
+        {
+            Program p = new Program();
+            FoodDetail m;
+            m = get(n);
+
+            var exception = Assert.Throws<Exception>(() => p.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, m));
+            Assert.AreEqual("Incorrect food request date. Please provide valid value", exception.Message);
+        }
+        [Test]
+        [TestCase(1, "2021/08/07", "abh", 20.00, 0)]
+        public void CreateSupplyDetail_ObjectNull(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge, int n)
+        {
+            Program p = new Program();
+            FoodDetail m;
+            m = get(n);
+            SupplyDetail res = p.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, m);
+            Assert.That(res, Is.EqualTo(null));
+        }
+
+    }
+}
